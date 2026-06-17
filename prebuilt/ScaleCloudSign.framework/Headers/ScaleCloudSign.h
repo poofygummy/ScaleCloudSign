bc23927de0c4c819f87da836e100f5b645cbcb09
PoofyGummy <poofygummy@gmail.com>
2026-06-17 22:42:39 +0200

    rebuilt SCSign

---
+++ prebuilt/ScaleCloudSign.framework/Headers/ScaleCloudSign.h
@@ -0,0 +1,39 @@
+//
+//  ScaleCloudSign.h
+//  ScaleCloudSign
+//
+//  Created by Riley Testut on 5/10/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+// Apple API
+#import <ScaleCloudSign/ALTAppleAPI.h>
+#import <ScaleCloudSign/ALTAppleAPISession.h>
+
+// Signing
+#import <ScaleCloudSign/ALTSigner.h>
+
+// Model
+#import <ScaleCloudSign/ALTApplication.h>
+#import <ScaleCloudSign/ALTAccount.h>
+#import <ScaleCloudSign/ALTAnisetteData.h>
+#import <ScaleCloudSign/ALTTeam.h>
+#import <ScaleCloudSign/ALTDevice.h>
+#import <ScaleCloudSign/ALTCertificate.h>
+#import <ScaleCloudSign/ALTAppID.h>
+#import <ScaleCloudSign/ALTAppGroup.h>
+#import <ScaleCloudSign/ALTProvisioningProfile.h>
+
+// Categories
+#import <ScaleCloudSign/NSError+ALTErrors.h>
+#import <ScaleCloudSign/NSFileManager+Zip.h>
+#import <ScaleCloudSign/NSCharacterSet+ASCII.h>
+
+// Capabilities
+#import <ScaleCloudSign/ALTCapabilities.h>
+
+//! Project version number for ScaleCloudSign.
+FOUNDATION_EXPORT double ScaleCloudSignVersionNumber;
+
+//! Project version string for ScaleCloudSign.
+FOUNDATION_EXPORT const unsigned char ScaleCloudSignVersionString[];
